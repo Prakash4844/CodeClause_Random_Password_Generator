0c76381ffccad20b918cceddb71e58b8565d8583
Prakash4844 <pk484442@gmail.com>
2023-05-02 22:04:15 +0530

    Added Numberlength and Symbollength Option
    Added Copy Option

--- Random_Pass_app.py
+++ Random_Pass_app.py
@@ -1,26 +1,25 @@
 import customtkinter as ctk
 from password_generator import PasswordGenerator
+
 MIN_UPPER = 0
 MIN_LOWER = 0
 MIN_NUM = 0
 MIN_SYM = 0
 
 
-def generate_password(pass_length, include_num=True, include_sym=True):
-
-    section = int(pass_length)//4
+def generate_password(pass_length, num, sym):
+    section = int(pass_length) // 4
 
     pwo = PasswordGenerator()
     # All properties are optional
     pwo.minlen = int(pass_length)
     pwo.maxlen = int(pass_length)
 
-    if not include_num:
-        pwo.minnumbers = 0
+    if not num == "":
+        pwo.minnumbers = int(num)
 
-    if not include_sym:
-        pwo.minsymbols = 0
-
+    if not sym == "":
+        pwo.minschars = int(sym)
 
     password = pwo.generate()
     print(password)
@@ -35,7 +34,6 @@ class App(ctk.CTk):
         self.title("Password Generator")
         self.geometry("900x300")
         self.resizable(False, False)
-        generate_password(10)
 
     def exit(self):
         self.destroy()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import customtkinter as ctk
 from Random_Pass_app import App, generate_password
+import pyperclip
 
 app = App()  # create CTk window like you do with the Tk window
 
@@ -9,83 +10,65 @@ label = ctk.CTkLabel(app, text="Generated Password")
 label.place(relx=0.5, rely=0.1, anchor=tk.CENTER)
 
 # create a text box
-text_box = ctk.CTkTextbox(app, width=800, height=50)
+text_box = ctk.CTkTextbox(app, width=700, height=50)
 text_box.place(relx=0.5, rely=0.25, anchor=tk.CENTER)
 Include_Numbers = True
 Include_Symbols = True
 
 
-def checkbox_eventA_Z():
-    pass
-
-
-check_varA_Z = ctk.StringVar(app, "on")
-print(check_varA_Z)
-checkboxA_Z = ctk.CTkCheckBox(app, text="A-Z", command=checkbox_eventA_Z,
-                              variable=check_varA_Z, onvalue="on", offvalue="off", state="disabled")
-checkboxA_Z.place(relx=0.05, rely=0.7)
-
-
-def checkbox_eventa_z():
-    pass
-
-
-check_vara_z = ctk.StringVar(app, "on")
-checkboxa_z = ctk.CTkCheckBox(app, text="a-z", command=checkbox_eventa_z,
-                              variable=check_vara_z, onvalue="on", offvalue="off", state="disabled")
-checkboxa_z.place(relx=0.2, rely=0.7)
-
-
-def checkbox_event_num():
-    state = checkbox_num.get()
-    if state == "on":
-        Include_Numbers = True
-    else:
-        Include_Numbers = False
-        print("exluded numbers")
-
-
-check_var_num = ctk.StringVar(app, "on")
-checkbox_num = ctk.CTkCheckBox(app, text="Numbers", command=checkbox_event_num,
-                               variable=check_var_num, onvalue="on", offvalue="off")
-checkbox_num.place(relx=0.05, rely=0.8)
-
-def checkbox_event_symbol():
-    state = checkbox_num.get()
-    if state == "on":
-        Include_Symbols = True
-    else:
-        Include_Symbols = False
-        print("exluded symbols")
-
-
-check_var_symbol = ctk.StringVar(app, "on")
-checkbox_symbol = ctk.CTkCheckBox(app, text="Symbols", command=checkbox_event_symbol(),
-                                  variable=check_var_symbol, onvalue="on", offvalue="off")
-checkbox_symbol.place(relx=0.2, rely=0.8)
-
 # create a buttonGenerate
-def button_function():
+def generate_function():
     text_box.configure(state="normal")
     text_box.delete("0.0", tk.END)
-    password = generate_password(pass_length_slider.get(), include_num=Include_Numbers,
-                                 include_sym=Include_Symbols)
-    print(password)
+    Nums = submit_function()[0]
+    Syms = submit_function()[1]
+    password = generate_password(pass_length_slider.get(), num=Nums,
+                                 sym=Syms)
+    # print(password)
     if len(password) > 36:
         text_box.configure(font=("Liberation Mono", 25))
     else:
-        text_box.configure(font=("Liberation Mono", 35))
+        text_box.configure(font=("Liberation Mono", 30))
     text_box.insert("0.0", password)
     text_box.configure(state="disabled")
     text_box.update()
 
 
-buttonGenerate = ctk.CTkButton(app, text="Generate Password", command=button_function)
+def copy_fuction():
+    copytext = text_box.get("0.0", tk.END)
+    pyperclip.copy(copytext)
+
+
+buttonCopy = ctk.CTkButton(app, text="Copy", command=copy_fuction, height=50, width=30)
+buttonCopy.place(relx=0.90, rely=0.25, anchor=tk.CENTER)
+
+buttonGenerate = ctk.CTkButton(app, text="Generate Password", command=generate_function, height=30, width=30
+                                , font=("Noto Sans", 15))
 buttonGenerate.place(relx=0.3, rely=0.5, anchor=tk.CENTER)
 
-buttonExit = ctk.CTkButton(app, text="Quit", command=app.exit)
+buttonExit = ctk.CTkButton(app, text="Quit", command=app.exit, height=30, width=150, font=("Noto Sans", 15))
 
 buttonExit.place(relx=0.7, rely=0.5, anchor=tk.CENTER)
+
+# create a label
+Numlabel = ctk.CTkLabel(app, text="Minimum Numbers in Password")
+Numlabel.place(relx=0.15, rely=0.7, anchor=tk.CENTER)
+Num_entry = ctk.CTkEntry(app, width=50, placeholder_text="3")
+Num_entry.place(relx=0.15, rely=0.8, anchor=tk.CENTER)
+
+# create a label
+Symlabel = ctk.CTkLabel(app, text="Minimum Symbols in Password")
+Symlabel.place(relx=0.45, rely=0.7, anchor=tk.CENTER)
+Sym_entry = ctk.CTkEntry(app, width=50, placeholder_text="3")
+Sym_entry.place(relx=0.45, rely=0.8, anchor=tk.CENTER)
+
+
+def submit_function():
+    Nums = Num_entry.get()
+    Syms = Sym_entry.get()
+    print(Sym_entry.get())
+    print(Num_entry.get())
+    return [Nums, Syms]
 
 
 def pass_length_slider_event(value):
@@ -95,11 +78,11 @@ def pass_length_slider_event(value):
     return round(value)
 
 
-pass_length_slider = ctk.CTkSlider(app, from_=8, to=64, command=pass_length_slider_event)
-pass_length_slider.place(relx=0.5, rely=0.9, anchor=tk.CENTER)
+pass_length_slider = ctk.CTkSlider(app, from_=8, to=64, command=pass_length_slider_event, width=300)
+pass_length_slider.place(relx=0.8, rely=0.8, anchor=tk.CENTER)
 pass_length_slider.set(12)
 pass_length_slider_label = ctk.CTkLabel(app, bg_color="transparent", text=f"Password Length = 12")
-pass_length_slider_label.place(relx=0.5, rely=0.82, anchor=tk.CENTER)
+pass_length_slider_label.place(relx=0.8, rely=0.7, anchor=tk.CENTER)
 pass_length_slider_label.update()
 
 app.mainloop()
